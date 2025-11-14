71d0588e4147452fe6cc3eb88717f1cd7a7dd7a6
Eyad Farah <eyadkhfarah@gmail.com>
2025-11-14 23:34:24 +0200

    Update social

--- src/lib/SocialLinks.tsx
+++ src/lib/SocialLinks.tsx
@@ -1,7 +1,7 @@
 export const SocialLink = {
   facebook: "https://www.facebook.com/Cyber.Hunter.Group",
   instagram: "https://www.instagram.com/cyberhuntergp",
-  linkedin: "https://www.linkedin.com/company/cyper-hunter-group",
+  linkedin: "https://www.linkedin.com/company/cyber-hunter-group/",
   twitter: "https://www.twitter.com/CyberHunter_Gp",
   tiktok: "https://www.tiktok.com/@cyber.hunter.gp",
   email: "info@cyberhuntereg.com",
